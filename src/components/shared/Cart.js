0afd79e59fb7ec9839f61a79282e5189339c71f3
amirthenew <ah.abshari@hotmail.com>
2023-02-08 22:20:10 +0330

    cart component created

--- src/components/shared/Cart.js
+++ src/components/shared/Cart.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React ,{useContext}from 'react';
 
 
-const Cart = () => {
+//context
+import { CartContext } from '../../context/CartContextProvider';
+
+//functions
+import { shorten } from '../../helper/functions';
+
+const Cart = (props) => {
+    const {dispatch} = useContext(CartContext)
+    const {image,title,price,quantity} = props.data
     return ( <div>
+        <img src={image} alt='product'/>
+        <div>
+        <p>{price} $</p>
+        <span>{quantity}</span>
+        <h3>{shorten(title)}</h3>
+        </div>
+
+        <div>
         
+        </div>
         </div> );
 }
  
