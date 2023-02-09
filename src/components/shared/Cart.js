c29bc4f90615d2dbfc8a7a7cccacff25b7419fbe
amirthenew <ah.abshari@hotmail.com>
2023-02-10 00:23:38 +0330

    cart styling finished

--- src/components/shared/Cart.js
+++ src/components/shared/Cart.js
@@ -9,23 +9,25 @@ import { CartContext } from '../../context/CartContextProvider';
 //functions
 import { shorten } from '../../helper/functions';
 
+import styles from './Cart.module.css'
+
 const Cart = (props) => {
     const {dispatch} = useContext(CartContext)
     const {image,title,price,quantity} = props.data
     return ( 
-        <div>
+        <div className={styles.container}>
 
 
-        <img src={image} alt='product'/>
-        <div>
+        <img className={styles.productImage} src={image} alt='product'/>
+        <div className={styles.data}>
         <h3>{shorten(title)}</h3>
         <p>{price} $</p>
         </div>
         
         <div>
-        <span>{quantity}</span>
+        <span className={styles.quantity}>{quantity}</span>
         </div>
-<div>
+<div className={styles.buttonContainer}>
         {
             quantity>1 ? <button onClick={()=>dispatch({type:'DECREASE',payload : props.data})}> - </button> :
             <button onClick={()=>dispatch({type:'REMOVE_ITEM',payload : props.data})}><img style={{width:'20px'}} src={trashIcon}/></button>
