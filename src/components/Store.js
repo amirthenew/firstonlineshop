23ce5ffa25de0e796fb0ea0c82e8eeb19b342a16
amirthenew <ah.abshari@hotmail.com>
2023-02-10 00:51:53 +0330

    store styling

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,11 +1,12 @@
 import React,{useContext} from 'react';
 import Product from './shared/Product';
+import styles from './Store.module.css'
 //context
 import { ProductsContext } from '../context/ProductContextProvider';
 const Store = () => {
 const products = useContext(ProductsContext)
 
-    return (<div style={{display:'flex',flexWrap:'wrap',justifyContent:'space-between'}}>
+    return (<div className={styles.container} style={{display:'flex',flexWrap:'wrap',justifyContent:'space-between'}}>
 
         {products.map( product=><Product 
             key={product.id} 
